fb485098bbaafe8d42d353ea461b87e50b7d0d80
Wesley Damasceno <wesleydmscn.co@gmail.com>
2023-03-13 21:27:22 -0300

    build(utils): add social property in object psychologists

--- src/utils/psychologists.ts
+++ src/utils/psychologists.ts
@@ -8,20 +8,23 @@ export const psychologists = [
     name: 'Antônia Liandra da Silva',
     crp: 'CRP 11/19442',
     description: 'Sou psicóloga graduada pela faculdade Ieducare-Fied/UNINTA Tianguá e pós graduanda em Saúde Mental pela faculdade Dom Alberto. Atuo com base na psicanálise e meus estudos são guiados por temas como psicologia decolonial, políticas públicas, feminismo e antirracismo.',
-    profile_image: ProfileLiandra
+    profile_image: ProfileLiandra,
+    social: 'https://www.instagram.com/liandra.psi/'
   },
   {
     id: 2,
     name: 'Isabel Oliveira do Carmo',
     crp: 'CRP 11/19182',
     description: 'Sou Psicóloga formada pela Faculdade Ieducare-Fied e Pós-Graduanda em Psicologia e Saúde da Mulher pela Faculdade Metropolitana. Atuo com base na clínica da Psicologia Comportamental e tenho interesse por temas de Saúde Mental da Mulher envolvendo padrão estético, mídias sociais e socialização feminina.',
-    profile_image: ProfileIsabel
+    profile_image: ProfileIsabel,
+    social: 'https://instagram.com/isabeloliveiira'
   },
   {
     id: 3,
     name: 'Larissa Oliveira Abreu',
     crp: 'CRP 11/19185',
     description: 'Sou Psicóloga formada pela Faculdade Ieducare-Fied/Uninta Tianguá e Pós-Graduanda em Psicologia Social pela Faculdade UniVitoria. Atuo com base na clínica da Terapia Cognitivo-comportamental e tenho interesse por temas voltados para violência contra mulheres e políticas públicas.',
-    profile_image: ProfileLarissa
+    profile_image: ProfileLarissa,
+    social: 'https://instagram.com/psi_larissaaoliveira'
   }
 ]
